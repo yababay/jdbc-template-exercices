e5f902f3fa64a484d7b1f0929a943afd6563cf29
mabel <bigbelk@live.ru>
2022-08-23 15:50:52 +0300

    first

---
+++ src/main/java/SpringBootConsoleApplication.java
@@ -0,0 +1,30 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("ru.yababay.jdbc.config")
+@SpringBootApplication
+public class SpringBootConsoleApplication
+        implements CommandLineRunner {
+
+    private static Logger LOG = LoggerFactory
+            .getLogger(SpringBootConsoleApplication.class);
+
+    public static void main(String[] args) {
+        LOG.info("STARTING THE APPLICATION");
+        SpringApplication.run(SpringBootConsoleApplication.class, args);
+        LOG.info("APPLICATION FINISHED");
+    }
+
+    @Override
+    public void run(String... args) {
+        LOG.info("EXECUTING : command line runner");
+
+        for (int i = 0; i < args.length; ++i) {
+            LOG.info("args[{}]: {}", i, args[i]);
+        }
+    }
+}
---
+++ src/main/java/ru/yababay/jdbc/config/SpringJdbcConfig.java
@@ -0,0 +1,21 @@
+package ru.yababay.jdbc.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@Configuration
+@ComponentScan("ru.yababay.jdbc.config")
+public class SpringJdbcConfig {
+        @Bean
+        public DataSource dataSource() {
+            return new EmbeddedDatabaseBuilder()
+                    .setType(EmbeddedDatabaseType.H2)
+                    .addScript("classpath:jdbc/schema.sql")
+                    .addScript("classpath:jdbc/test-data.sql").build();
+        }
+}
